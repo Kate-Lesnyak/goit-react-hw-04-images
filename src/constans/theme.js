14d26a213e2f9cb88c36514c1ba905bf78d96a32
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-03-09 15:52:06 +0200

    instal components

---
+++ src/constans/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    primaryTextColor: '#212121',
+    bodyAndBtnColor: '#ffffff',
+    accentColor: '#3f25b5',
+    errorColor: '#b70208',
+    backgroundBtnAndSearchFormColor: '#3f51b5',
+    backgroundLoaderColor: 'rgba(0, 0, 0, 0.6)',
+    backgroundModalColor: 'rgba(0, 0, 0, 0.8)',
+  },
+
+  btnBoxShadow: '0px 3px 1px - 2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)',
+
+  galleryItemBoxShadow: '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px - 1px rgba(0, 0, 0, 0.12)',
+
+  searchBarBoxShadow: ' 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
+
+  transition: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
+}
